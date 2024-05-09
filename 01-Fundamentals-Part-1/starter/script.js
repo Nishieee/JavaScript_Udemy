3d4bbca8ffba05f3017eaee16de76a5952a37150
NOISHEE <matlaninishi@gmail.com>
2024-05-09 11:03:03 +0530

    Commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,64 @@
+/*let js = "amazing";
+if (js === "amazing") alert('js is fun')
+
+console.log(70 + 9 + 8 - 10);
+console.log("jonas");
+
+let firstname = "nishita";
+console.log(firstname);
+
+let myCountry = "India";
+let myContinent = "asis";
+let myPopulation = 5;
+
+console.log(myContinent)
+console.log(myCountry)
+console.log(myPopulation)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+console.log(typeof javascriptIsFun)
+
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 70;
+age = 31;
+console.log(age);
+
+*/
+
+const now = 2024;
+let ageNishi = now - 2002;
+let ageHriday = now - 2007
+console.log(ageNishi, ageHriday)
+
+console.log(ageNishi ** 2)
+
+const FirstName = 'Nishi';
+const LastName = 'Hriday';
+console.log(FirstName + ' ' + LastName)
+
+let x = 10 + 5;
+x += 10;
+x++
+console.log(x)
+
+console.log(ageNishi > ageHriday)
+console.log(ageHriday >= 18)
+
+const isAdult = ageNishi >= 18;
+console.log(now - 1991 > now - 2018)
+
+const jonas = "I'm " + FirstName + " " + LastName + " and I'm " + ageNishi + " Years old"
+console.log(jonas)
